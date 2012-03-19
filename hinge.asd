4bd16e9c93e441e7615979ae9709854ed1f061f1
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-03-19 05:25:58 -0400

    [server] Sketching out the TCP server implementation. Can now bind and notify of connections correctly.

--- hinge.asd
+++ hinge.asd
@@ -5,11 +5,13 @@
 (defsystem :hinge
   :description "A synonym for node. Something like an evented framework in and for CL."
   :version "0.0.0"
-  :depends-on (:log5 :ev)
+  :depends-on (:log5 :usocket :ev)
 
   :components ((:module "src" :components
                         ((:file "package")
                          (:file "hinge" :depends-on ("package"))
                          (:file "generics" :depends-on ("package"))
                          (:file "methods" :depends-on ("generics" "hinge"))
-                         (:file "emitter" :depends-on ("hinge" "generics"))))))
+                         (:file "emitter" :depends-on ("hinge" "generics"))
+
+                         (:file "server" :depends-on ("emitter"))))))
---
+++ src/server.lisp
@@ -0,0 +1,55 @@
+(in-package :hinge)
+
+;; Helper
+(defun socket-fd (socket)
+  "Translate a heavy `socket' object to a numeric file descriptor."
+  (sb-bsd-sockets:socket-file-descriptor (usocket:socket socket)))
+
+;; Class
+(defclass server (emitter)
+  ((acceptor :initform (vector nil nil)
+             :accessor acceptor
+             :documentation "The listen socket of the server and the watcher for it as a 2-vector.")
+   (peers :initform (list)
+          :accessor peers
+          :documentation "Sockets of any accepted peers.")))
+
+;; Generics -- User API
+(defgeneric bind (server port &optional host)
+  (:documentation "Start listening for incoming connections for this `server' on the
+given `port' and `host', if given. If `host' the server will listen on any interface."))
+(defgeneric connection (server)
+  (:documentation "Called when a new connection is signaled on `server'.
+The connection is accepted and emitted with the `connection' event."))
+
+;; Methods
+(defmethod bind ((server server) (port number) &optional (host usocket:*wildcard-host*))
+  (let* ((sock (usocket:socket-listen host port :reuse-address t))
+         (watcher (make-instance 'ev:ev-io-watcher)))
+
+    (ev:set-io-watcher *hinge* watcher (socket-fd sock) ev:EV_READ
+                       #'(lambda (l w e)
+                           (declare (ignore l w e))
+                           (connection server)))
+    (ev:start-watcher *hinge* watcher)
+
+    (setf (svref (acceptor server) 0) sock
+          (svref (acceptor server) 1) watcher)
+
+    (prog1 server
+      (emit server "listening" server))))
+
+(defmethod connection ((server server))
+  (let ((peer-sock (usocket:socket-accept (svref (acceptor server) 0))))
+    (push peer-sock (peers server))
+    (prog1 server
+      (emit server "connection" peer-sock))))
+
+(defmethod close ((server server) &key abort)
+  "Close the accepting socket to prevent further connections
+from arriving."
+  (declare (ignore abort))
+  (when (acceptor server)
+    ;; TODO: Stop the watcher
+    (usocket:socket-close (acceptor server))
+    (emit server "closed" server)))
