28c1e9c4dedeef80dc2bfb41d6893d89c0b9b8b0
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-05-01 20:26:39 -0400

    [zmq-socket] Allow the fetching of the default hinge to reset the hinge

--- src/hinge.lisp
+++ src/hinge.lisp
@@ -2,7 +2,10 @@
 
 ;; Dynamic default hinge
 (defvar *hinge* nil "The default hinge reactor.")
-(defun get-default-hinge ()
+(defun get-default-hinge (&optional reset)
+  (when reset
+    (setf *hinge* nil))
+
   (or *hinge*
       (setf *hinge* (make-instance 'hinge))))
 
@@ -10,6 +13,3 @@
 (defclass hinge (ev:ev-loop)
   ((bg-pool :accessor bg-pool
             :documentation "Background work threadpool")))
-
-
-
