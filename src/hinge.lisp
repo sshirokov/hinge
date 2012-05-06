c80a5c3ec20564010cd246dbe31303c33eaceefc
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-05-06 15:17:55 -0400

    [socket-data-vs-connection-race] Queues and runner slots for the event and delivery queues

--- src/hinge.lisp
+++ src/hinge.lisp
@@ -13,4 +13,18 @@
 ;; Classes
 (defclass hinge (ev:ev-loop)
   ((bg-pool :accessor bg-pool
-            :documentation "Background work threadpool")))
+            :documentation "Background work threadpool")
+
+   (emit-queue :accessor emit-queue
+               :initform (make-instance 'queue)
+               :documentation "Queue of event emissions.")
+   (emit-runner :initform nil
+                :accessor emit-runner
+                :documentation "The queue runner of the `emit-queue'")
+
+   (deliver-queue :accessor deliver-queue
+                  :initform (make-instance 'queue)
+                  :documentation "Queue of event deliveries.")
+   (deliver-runner :initform nil
+                   :accessor deliver-runner
+                   :documentation "The queue runner of the `deliver-queue'")))
