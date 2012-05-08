9a5474a16a65a9fb9d923d4e348b079e826a4327
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-05-08 17:30:03 -0400

    [http] Updating example

--- examples/http.lisp
+++ examples/http.lisp
@@ -1,23 +1,16 @@
 (ql:quickload :hinge)
 (defpackage :hinge-example
   (:use :cl :hinge :hinge.http))
+
 (in-package :hinge-example)
 
 (let ((server (make-instance 'http-server)))
   (add-listener server "request"
-                (lambda (request)
-                (format t "Request: ~S~%" request)
-                (describe request)
+                (lambda (request response)
+                  (declare (ignorable request))
+                  (write-head response 200 '(("Content-Type" . "text/html")))
+                  (end response "Hello world!")))
 
-                (send (sock (peer request))
-                      (babel:string-to-octets
-                       (let ((reply "Hello world"))
-                         (concatenate 'string
-                                      (format nil "HTTP/1.1 200 OK~A~A" #\Return #\Newline)
-                                      (format nil "Connection: close~A~A" #\Return #\Newline)
-                                      (format nil "Content-Length: ~A~A~A" (length reply) #\Return #\Newline)
-                                      (format nil "~A~A~A" #\Return #\Newline reply))))
-                      (lambda (sock) (close sock)))))
   (bind server 4545))
 
 (run :default)
