204628c872ea13f6c9e2dc5e136f3dffaab00cb9
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-05-08 03:09:42 -0400

    [http] An example reply that works with the current request

--- examples/http.lisp
+++ examples/http.lisp
@@ -6,8 +6,18 @@
 (let ((server (make-instance 'http-server)))
   (add-listener server "request"
                 (lambda (request)
-                  (format t "Request: ~S~%" request)
-                  (describe request)))
+                (format t "Request: ~S~%" request)
+                (describe request)
+
+                (send (sock (peer request))
+                      (babel:string-to-octets
+                       (let ((reply "Hello world"))
+                         (concatenate 'string
+                                      (format nil "HTTP/1.1 200 OK~A~A" #\Return #\Newline)
+                                      (format nil "Connection: close~A~A" #\Return #\Newline)
+                                      (format nil "Content-Length: ~A~A~A" (length reply) #\Return #\Newline)
+                                      (format nil "~A~A~A" #\Return #\Newline reply))))
+                      (lambda (sock) (close sock)))))
   (bind server 4545))
 
 (run :default)
