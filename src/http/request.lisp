6ece61ebdab3e0c3a2d708754f45490414781ebd
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-05-08 17:09:43 -0400

    [http] Response functions are go

--- src/http/request.lisp
+++ src/http/request.lisp
@@ -27,6 +27,10 @@ the reason, if it is a string, otherwise a stock reason for the given code.
 If `reason/headers' is a list or `headers' is given a call to `set-headers'
 is made, adding them to the response headers."))
 
+(defgeneric write-headers (response &optional headers)
+  (:documentation "Add any `headers' to the response headers
+of `response' then write the header list to the client."))
+
 (defgeneric set-headers (response headers)
   (:documentation "Update the outgoing headers on `response'
 by adding the headers in the `headers' alist, replacing any
@@ -62,12 +66,27 @@ or if the parser machinery of the peer needs to be reloaded."))
                    (otherwise (or headers ())))))
 
     (send (sock (peer (request response)))
-          (babel:octets-to-string
+          (babel:string-to-octets
            (format nil "~A ~A ~A~A~A" (version (request response)) status reason #\Return #\Newline)))
     (setf (status-sent response) t)
 
     (when headers
       (set-headers response headers))))
+
+(defmethod write-headers ((response http-response) &optional headers)
+  (when headers
+    (set-headers response headers))
+
+  (mapc #'(lambda (header-pair)
+            (destructuring-bind (key . value) header-pair
+              (send (sock (peer (request response)))
+                    (babel:string-to-octets
+                     (format nil "~A: ~A~A~A" key value #\Return #\Newline)))))
+        (headers response))
+  (send (sock (peer (request response)))
+                    (babel:string-to-octets
+                     (format nil "~A~A" #\Return #\Newline)))
+  (setf (headers-sent response) t))
 
 (defmethod set-headers ((response http-response) headers)
   (mapc #'(lambda (header-pair)
@@ -87,15 +106,51 @@ or if the parser machinery of the peer needs to be reloaded."))
 (defmethod header ((response http-response) (key string))
   (cdr (assoc key (headers response) :test #'string-equal)))
 
+(defmethod begin ((response http-response))
+  (unless (status-sent-p response)
+    (write-head 200 "OK"))
+
+  (unless (headers-sent-p response)
+    (write-headers response)))
+
 (defmethod send ((response http-response) data &optional callback)
   (declare (ignore callback))
-  "Send a chunk to the client. If this is the first chunk
-and there is no content length set, or no chunked encoding
-header in the headers of the response a content-length header
-is stored before the write. If a chunked encoding header is found
-in the headers of the response the `data' is written as
-an HTTP chunk.
+  "Send a chunk to the client.
+
+If this is the first chunk and there is no content length set,
+or no chunked encoding set in the headers of the response a
+content-length header is stored before the write.
+
+If a chunked encoding header is found in the headers of the response
+the `data' is written as an HTTP chunk.
+
+No verification is made that the `data' does not exceed the advertised
+content length if one is present.
 
 If the response code or headers haven't been written yet, they are
 written first."
-  :TODO)
+  (let ((bindata (if (stringp data) (babel:string-to-octets data) data)))
+    (unless (or (header response "Content-Length")
+                (string-equal (header response "Transfer-Encoding") "chunked"))
+      (setf (header response "Content-Length") (length bindata)))
+
+    (begin response)
+
+    (if (string-equal (header response "Transfer-Encoding") "chunked")
+        (format t "TODO: Chunked encoding is not supported")
+        (send (sock (peer (request response)))
+              bindata))))
+
+(defmethod end ((response http-response) &optional data)
+  (if data
+      (send response data)
+      (begin response))
+
+  (if (string-equal (header response "Connection") "close")
+      (add-listener (sock (peer (request response))) "drain"
+                    (lambda (sock)
+                      (format t "~S Closing connection after body send.~%" (peer (request response)))
+                      (close sock)))
+
+      (setf (parser (peer (request response)))
+            (make-instance 'request-parser :peer (peer (request response))))))
