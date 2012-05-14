471f37baa8cb7ede114607a0fb33eafb74829755
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-05-14 17:53:04 -0400

    [untimer-defer] Starting to wrap up a queue drained by a watcher.

--- src/hinge.lisp
+++ src/hinge.lisp
@@ -11,6 +11,25 @@
       (setf *hinge* (make-instance 'hinge))))
 
 ;; Classes
+;;; Queue drained by an idle runner
+(defclass running-queue (c2mop:funcallable-standard-object)
+  ((owner :initarg :owner :accessor owner)
+
+   (queue :initform (make-instance 'queue) :accessor queue)
+   (priority :initform 0 :accessor priority)
+   (runner :initform (make-instance 'ev:ev-idle) :accessor runner))
+  (:metaclass c2mop:funcallable-standard-class)
+  (:documentation "A wrapper that binds together a queue and an idle runner
+to process it."))
+
+(defgeneric event-callback (inst l w e)
+  (:documentation "Callback curried and submitted to libev for the `runner' of `inst'"))
+
+(defmethod initialize-instance :after ((inst running-queue) &key)
+  (c2mop:set-funcallable-instance-function inst (curry #'event-callback inst))
+  (ev:set-idle (owner inst) (runner inst) inst))
+
+;;; Hinge
 (defclass hinge (ev:ev-loop)
   ((bg-pool :accessor bg-pool
             :documentation "Background work threadpool")
